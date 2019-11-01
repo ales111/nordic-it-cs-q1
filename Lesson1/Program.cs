e0467ceb9b8964d78902689f0d4dcf671033be81
Alexandr <alicegracework@mail.ru>
2019-11-01 05:54:40 +0300

    homework

---
+++ Lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine("Привет, {0}", name);
+            Thread.Sleep(5000);
+            Console.WriteLine("Досвидания, {0}", name);
+            Console.ReadKey();
+        }
+    }
+}
