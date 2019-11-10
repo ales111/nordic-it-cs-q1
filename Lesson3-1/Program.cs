4f1d3c16e0b6b73de9510aebd6e68c0e56232d6e
Alexandr <alicegracework@mail.ru>
2019-11-10 16:39:51 +0300

    Homework

---
+++ Lesson3-1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] array2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for (int i = 0; i < array1.Length; i++) {
+                string tableline = "";
+
+            
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    int result = array1[i] * array2[j];
+
+                    tableline = tableline + " " + result;
+                    
+
+
+                }
+                Console.WriteLine(tableline);
+
+            }
+
+            
+
+
+        }
+    }
+}
---
+++ Lesson3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = new string[3];
+        int[] ages = new int[3];
+
+         for (int i = 0; i < ages.Length; i++)
+        {
+            Console.Write("Введите имя: ");
+            names[i] = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            ages[i] = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+ 
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine("Name: " + names[i] + ", age in 4 years: " + ages[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
