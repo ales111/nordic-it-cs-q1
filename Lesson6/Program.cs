67b3525a0125824e6ed00354d7872259010d841a
Alexandr <alicegracework@mail.ru>
2019-11-19 00:18:04 +0300

    Homework

---
+++ Lesson6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+        int count = 0;
+
+
+        do
+        {
+            Console.Write("\nВведите положительное натуральное число не более 2 миллиардов: ");
+            int.TryParse(Console.ReadLine(), out num);
+            if (num < 10)
+            {
+                Console.Write("Введено неверное значение! Попробуйте ещё раз: ");
+            }
+            else break;
+
+        } while (true);
+
+        do
+        {
+
+            if (num % 2 == 0) count++;
+            num = num/10;
+        } while (num > 0);
+
+        Console.WriteLine("Количество четных чисел равно {0}", count);
+
+
+
+    }
+}
