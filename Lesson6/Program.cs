faeecf72141c690e0d30e20548bab1382f629e5c
Alexandr <alicegracework@mail.ru>
2019-11-26 00:50:35 +0300

    Homework

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -9,29 +9,45 @@ class Program
         int num;
         int count = 0;
 
-
-        do
         {
-            Console.Write("\nВведите положительное натуральное число не более 2 миллиардов: ");
-            int.TryParse(Console.ReadLine(), out num);
-            if (num < 10)
+            do
             {
-                Console.Write("Введено неверное значение! Попробуйте ещё раз: ");
-            }
-            else break;
 
-        } while (true);
 
-        do
-        {
+                Console.Write("\nВведите положительное натуральное число не более 2 миллиардов: ");
+                int.TryParse(Console.ReadLine(), out num);
+                if (num > 0) break;
+            } while (true);
+            do
 
-            if (num % 2 == 0) count++;
-            num = num/10;
-        } while (num > 0);
+                try
+                {
 
-        Console.WriteLine("Количество четных чисел равно {0}", count);
+                    if (num > 2000000000) continue;
 
 
 
+                }
+                catch (OverflowException)
+                {
+
+                    Console.Write("Ошибка");
+                    continue;
+                } while (num > 2000000000);
+
+        }
+
+
+                do
+                {
+                    if (num % 2 == 0) count++;
+                    num = num / 10;
+                } while (num > 0);
+
+               Console.WriteLine("Количество четных чисел равно {0}", count);
+
+
+
+          
     }
 }
---
+++ Lesson8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lesson8
+{
+    public class Human
+    {
+        protected string name;
+        public string Name
+        {
+            get { return name; }
+        }
+        protected int age;
+        public int Age
+        {
+            get { return age; }
+        }
+        public Human(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human[] array = new Human[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Enter name: ");
+                string imya = Console.ReadLine();
+                Console.Write("Enter age: ");
+                int vozrast = Convert.ToInt32(Console.ReadLine());
+                array[i] = new Human(imya, vozrast);
+
+
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Name: " + array[i].Name + ", age in 4 years: " + array[i].Age);
+            }
+            Console.Write("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
